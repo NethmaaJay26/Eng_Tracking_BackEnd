e5236572bc51eee220ca737e5e29deaff9fbae5d
kavindudp <kavindudananjaya02@gmail.com>
2024-10-23 18:45:30 +0530

    Normal eng fetch

--- routes/SengineerRoutes.js
+++ routes/SengineerRoutes.js
@@ -1,6 +1,7 @@
 // routes/SengineerRoutes.js
 
 import express from 'express';
+import Sengineer from '../models/SengineerModel.js';
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -12,6 +13,7 @@ import {
   updateSupervisingEngineer, 
   deleteSupervisingEngineer 
 } from '../controllers/SengineerController.js';
+// import { authenticateUse2 }  from '../middleware/authmiddleware.js'; 
 
 const sengineerrouter = express.Router();
 
@@ -47,5 +49,19 @@ sengineerrouter.delete('/:id', deleteSupervisingEngineer);
 
 // Route to login a supervising engineer
 sengineerrouter.post("/login", loginSengineer);
+
+// sengineerrouter.get('/me', authenticateUse2, async (req, res) => {
+//   try {
+//     const sengineer = await Sengineer.findById(req.user.id).select('name photo');
+//     if (!sengineer) {
+//       return res.status(404).json({ message: 'Supervising Engineer not found' });
+//     }
+//     res.status(200).json(sengineer);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).json({ message: 'Server Error' });
+//   }
+// });
+
 
 export default sengineerrouter;
